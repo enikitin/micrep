11edfa55bfa489ac3ad04fc238b1c832d6657928
enikitin <epnikitin@gmail.com>
2015-05-05 19:23:44 +0300

    Code committed to GitHub.

---
+++ js/slider.settings.js
@@ -0,0 +1,8 @@
+﻿$(window).load(function () {
+    $('#slider').nivoSlider({
+        effect: 'fade', //Specify sets like: 'fold,fade,sliceDown'
+        animSpeed: 500, //Slide transition speed
+        pauseTime: 10000, // How long each slide will show
+        startSlide: 0 //Set starting Slide (0 index)
+    });
+});
