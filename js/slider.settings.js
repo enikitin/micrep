56745d10747460d7230b204132be6fc2a802f9c9
enikitin <epnikitin@gmail.com>
2015-05-14 10:40:28 +0300

    Delete everything

--- js/slider.settings.js
+++
@@ -1,8 +0,0 @@
-﻿$(window).load(function () {
-    $('#slider').nivoSlider({
-        effect: 'fade', //Specify sets like: 'fold,fade,sliceDown'
-        animSpeed: 500, //Slide transition speed
-        pauseTime: 10000, // How long each slide will show
-        startSlide: 0 //Set starting Slide (0 index)
-    });
-});
