e58353a9f94d51c8f8fa46b2bc2c84d13219c09a
enikitin <epnikitin@gmail.com>
2015-05-14 10:43:44 +0300

    Totally rewritten

---
+++ js/app.js
@@ -0,0 +1,18 @@
+﻿angular
+    .module('app', ['ngRoute'])
+    .config([
+        '$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+            'use strict';
+
+            $locationProvider.hashPrefix('!');
+            
+            $routeProvider
+                .when('/en', { templateUrl: 'include/en/home.html' })
+                .when('/en/about', { templateUrl: 'include/en/about.html' })
+                .when('/en/partners', { templateUrl: 'include/en/partners.html' })
+                .when('/en/contact', { templateUrl: 'include/en/contact.html' })
+                .when('/ru', { templateUrl: 'include/ru/home.html' })
+                .when('/ru/partners', { templateUrl: 'include/ru/partners.html' })
+                .otherwise({ redirectTo: '/en' });
+        }
+    ]);
---
+++ js/topMenuCtrl.js
@@ -0,0 +1,27 @@
+﻿angular
+    .module('app')
+    .controller('topMenuCtrl', [
+        '$scope', '$location', '$window', function ($scope, $location, $window) {
+            'use strict';
+
+            function currentLanguage() {
+                return $location.path().split('/')[1];
+            }
+
+            $scope.menuClass = function (page) {
+                var current = $location.path().substring(4);
+                return page === current ? 'active' : '';
+            };
+
+            $scope.changeLanguage = function () {
+                var language = currentLanguage() === 'en' ? 'ru' : 'en';
+                //$window.location = '/' + language;
+                $location.path('/' + language);
+            };
+
+            $scope.menuUrl = function () {
+                var language = currentLanguage();
+                return '/include/' + language + '/topmenu.html';
+            };
+        }
+    ]);
