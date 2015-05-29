46590dbe6c34b9b2c5a4df020411816fc572b76b
enikitin <epnikitin@gmail.com>
2015-05-29 20:04:24 +0300

    Added grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+﻿module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        htmlSnapshot: {
+            debug: {
+                options: {
+                    snapshotPath: 'snapshots/',
+                    sitePath: 'http://localhost:777/',
+                    msWaitForPages: 1000,
+                    urls: [
+                        '/',
+                        '/partners'
+                    ]
+                }
+            },
+            prod: {
+                options: {}
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-html-snapshot');
+
+    grunt.registerTask('default', ['htmlSnapshot']);
+}
